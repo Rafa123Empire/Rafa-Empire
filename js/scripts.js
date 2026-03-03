0776d3bd6ad4865d22a7d456a48b7f9a66d88f58
DomJcbs <domajcbs1@gmail.com>
2026-03-03 21:24:47 +0200

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+﻿function submitForm(event) {
+  event.preventDefault();
+
+  const form = event.target;
+  const submitButton = form.querySelector("button[type='submit']");
+  const originalLabel = submitButton.textContent;
+
+  submitButton.textContent = "Message Sent";
+  submitButton.disabled = true;
+
+  alert("Thank you! Your message has been sent.");
+
+  form.reset();
+
+  window.setTimeout(function () {
+    submitButton.textContent = originalLabel;
+    submitButton.disabled = false;
+  }, 1800);
+}
